885c5b2697d886e09b56064e91ca7dfad7ba05c3
Calebe Carvalho <calebecvsts@gmail.com>
2026-05-13 21:07:21 -0300

    dever de casa aula1

--- index.js
+++ index.js
@@ -1,41 +1,6 @@
+const primeiroNumero = 100;
+const segundoNumero = 200;
 
-const http = require('http');
+const resultadoSoma = primeiroNumero + segundoNumero;
 
-const produtos = [
-    'Notebook',
-    'Mouse',
-    'Teclado',
-    'Monitor',
-    'Placa de Vídeo'
-];
-
-const server = http.createServer((req, res) => {
-    const listaProdutos = produtos
-        .map((produto) => `<li>${produto}</li>`)
-        .join('');
-
-    const html = `
-    <!DOCTYPE html>
-    <html lang="pt-br">
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Teste Node.js</title>
-    </head>
-    <body>
-      <h1>Lista de Peças</h1>
-
-      <ul>
-        ${listaProdutos}
-      </ul>
-    </body>
-    </html>
-  `;
-
-    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-    res.end(html);
-});
-
-server.listen(3000, () => {
-    console.log('Servidor rodando em http://localhost:3000');
-});
+console.log(resultadoSoma);
