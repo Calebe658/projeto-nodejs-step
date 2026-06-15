7e8e85dece234844fb0f36f9239a433c9639a793
Calebe Carvalho <calebecvsts@gmail.com>
2026-06-15 20:39:02 -0300

    implementa autenticacao JWT e protecao de rotas

--- src/routes/produtoRoutes.js
+++ src/routes/produtoRoutes.js
@@ -1,21 +1,16 @@
-const express = require('express');
+const express = require("express");
+
 const router = express.Router();
 
 const {
-    listarProduto,
-    criarProduto
-} = require(
-    '../controllers/produtoController'
-);
+    listarProdutos,
+    criarProduto,
+} = require("../controllers/produtoController");
 
-router.get(
-    '/produtos',
-    listarProduto
-);
+const autenticar = require("../middlewares/autenticar");
 
-router.post(
-    '/produtos',
-    criarProduto
-);
+router.post("/produtos", autenticar, criarProduto);
+
+router.get("/produtos", listarProdutos);
 
 module.exports = router;
