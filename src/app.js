cf4a104df57b957d335a49db390853f3d59d0274
Calebe Carvalho <calebecvsts@gmail.com>
2026-06-22 20:17:38 -0300

    implementa download com streams e compressao com zlib

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const usuarioRoutes = require("./routes/usuarioRoutes");
 const produtoRoutes = require("./routes/produtoRoutes");
 const arquivoRoutes = require("./routes/arquivoRoutes");
 const authRoutes = require("./routes/authRoutes");
+const streamRoutes = require("./routes/streamRoutes");
 
 const loggerMiddleware = require("./middlewares/loggerMiddleware");
 
@@ -17,5 +18,6 @@ app.use(authRoutes);
 app.use(produtoRoutes);
 app.use(usuarioRoutes);
 app.use(arquivoRoutes);
+app.use(streamRoutes);
 
 module.exports = app;
