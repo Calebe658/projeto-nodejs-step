915bd5104b1e111445b5736e5ef9cbf6444f1c11
Calebe Carvalho <calebecvsts@gmail.com>
2026-07-14 18:29:43 -0300

    cria api completa do projeto final

---
+++ src/database/connect.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+let cached = global.mongoose;
+
+if (!cached) {
+    cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function conectar() {
+    if (cached.conn) return cached.conn;
+
+    if (!cached.promise) {
+        const MONGODB_URL =
+            process.env.MONGODB_URL ||
+            "mongodb://localhost:27017/projeto-final";
+
+        cached.promise = mongoose.connect(MONGODB_URL);
+    }
+
+    cached.conn = await cached.promise;
+    return cached.conn;
+}
+
+async function desconectar() {
+    if (cached.conn) {
+        await mongoose.disconnect();
+        cached.conn = null;
+        cached.promise = null;
+    }
+}
+
+module.exports = { conectar, desconectar };
